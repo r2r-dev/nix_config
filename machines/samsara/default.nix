a7b9ce2208136d29460c534b29300430ef8c7572
Artur Stachecki <dev@r2r.sh>
2026-06-17 20:49:29 +0200

    chore: modernize sleep config

--- machines/samsara/default.nix
+++ machines/samsara/default.nix
@@ -69,13 +69,12 @@
     enable = true;
   };
 
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
-
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = false;
+    AllowHibernation = false;
+    AllowHybridSleep = false;
+    AllowSuspendThenHibernate = false;
+  };
 
   virtualisation.docker.storageDriver = "btrfs";
 
